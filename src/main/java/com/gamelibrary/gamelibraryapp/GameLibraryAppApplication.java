6682ed78c7252fae7cc539a3efd6148e6a8edeaa
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-03 15:58:51 -0500

    setting up repo

---
+++ src/main/java/com/gamelibrary/gamelibraryapp/GameLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.gamelibrary.gamelibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameLibraryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gamelibrary/gamelibraryapp/GameLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelibrary.gamelibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
