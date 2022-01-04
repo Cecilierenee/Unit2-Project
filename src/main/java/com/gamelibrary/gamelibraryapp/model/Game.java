32095cf003cbe3a4ce16b9311bfb4bdea6ffecb5
Brandon Nguyen <rmpumaz@gmail.com>
2022-01-04 10:55:27 -0500

    added initial private variables to game model

--- src/main/java/com/gamelibrary/gamelibraryapp/model/Game.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/model/Game.java
@@ -1,4 +1,62 @@
 package com.gamelibrary.gamelibraryapp.model;
 
 public class Game {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String releaseDate;
+    private char rating;
+
+    public Game() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public char getRating() {
+        return rating;
+    }
+
+    public void setRating(char rating) {
+        this.rating = rating;
+    }
 }
