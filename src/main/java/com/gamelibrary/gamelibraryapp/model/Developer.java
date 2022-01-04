5f5a390670d23315c55d2fd88faab32d400e9994
Brandon Nguyen <rmpumaz@gmail.com>
2022-01-04 10:58:04 -0500

    added initial private variables to developer model

--- src/main/java/com/gamelibrary/gamelibraryapp/model/Developer.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/model/Developer.java
@@ -1,4 +1,26 @@
 package com.gamelibrary.gamelibraryapp.model;
 
 public class Developer {
+
+    private Long id;
+    private String name;
+
+    public Developer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
