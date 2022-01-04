1afe422e7d23c3f896b813d6548a817e2d9908b3
Brandon Nguyen <rmpumaz@gmail.com>
2022-01-04 11:05:11 -0500

    InformationNotFoundException created

--- src/main/java/com/gamelibrary/gamelibraryapp/exception/InformationNotFoundException.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/exception/InformationNotFoundException.java
@@ -1,4 +1,10 @@
 package com.gamelibrary.gamelibraryapp.exception;
 
-public class InformationNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+    public  InformationNotFoundException(String message) {super(message);}
 }
