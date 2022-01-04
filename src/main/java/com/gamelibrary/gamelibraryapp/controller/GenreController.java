f60b31f963a2766f1b7e8579857bda7405dae84f
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-04 13:32:23 -0500

    add path to getgenres

--- src/main/java/com/gamelibrary/gamelibraryapp/controller/GenreController.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/controller/GenreController.java
@@ -4,6 +4,7 @@ package com.gamelibrary.gamelibraryapp.controller;
 import com.gamelibrary.gamelibraryapp.model.Genre;
 import com.gamelibrary.gamelibraryapp.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,8 @@ public class GenreController {
     private static final Logger LOGGER = Logger.getLogger(GenreController.class.getName());
     private GenreRepository genreRepository;
 
+
+@GetMapping(path = "/genre")
 public List<Genre> getGenres() {
     LOGGER.info("Calling getGenre method from controller");
     return genreRepository.findAll();
