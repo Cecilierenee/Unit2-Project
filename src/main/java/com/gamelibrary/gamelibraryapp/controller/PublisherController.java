f8b249224b9095597d70b64686563c40fde57c6f
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-05 17:48:03 -0500

    add getmapping to getpublishers method

--- src/main/java/com/gamelibrary/gamelibraryapp/controller/PublisherController.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/controller/PublisherController.java
@@ -5,6 +5,7 @@ import com.gamelibrary.gamelibraryapp.model.Publisher;
 import com.gamelibrary.gamelibraryapp.repository.PublisherRepository;
 import com.gamelibrary.gamelibraryapp.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class PublisherController {
         this.publisherService = publisherService;
     }
 
-
+    @GetMapping(path = "/publisher")
     public List<Publisher> getPublishers(){
         LOGGER.info("Calling getPublisher method from controller");
         return publisherService.getPublishers();
