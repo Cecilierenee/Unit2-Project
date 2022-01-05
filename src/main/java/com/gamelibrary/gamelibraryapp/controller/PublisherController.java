a74873bb08cd6def6a50640f22a193ef7712d862
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-05 17:38:34 -0500

    dependency injection

--- src/main/java/com/gamelibrary/gamelibraryapp/controller/PublisherController.java
+++ src/main/java/com/gamelibrary/gamelibraryapp/controller/PublisherController.java
@@ -1,4 +1,22 @@
 package com.gamelibrary.gamelibraryapp.controller;
 
+
+import com.gamelibrary.gamelibraryapp.repository.PublisherRepository;
+import com.gamelibrary.gamelibraryapp.service.PublisherService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path = "/api")
 public class PublisherController {
+
+    public static final Logger LOGGER = Logger.getLogger(PublisherController.class.getName());
+    private PublisherService publisherService;
+
+    public void setPublisherService(PublisherService publisherService){
+        this.publisherService = publisherService;
+    }
+
 }
